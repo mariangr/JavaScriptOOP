fc863d7a7ed98df50535d483acc2641049ffd3e5
Marian Graurov <marian_graurov@abv.bg>
2014-08-06 17:52:40 +0300

    initial commit

---
+++ 1.01 DOMManipulation/1.01 DOMManipulation/Script/script.js
@@ -0,0 +1,46 @@
+﻿var domModule = (function () {
+    var selectorBuffer = [];
+    var elementsBuffer = [];
+
+    function appendChild(element, select) {
+        $(select).append($(element));
+    }
+
+    function removeChild(parent, selector) {
+        selector = selector || "";
+        $(parent + " " + selector).remove();
+    }
+
+    function addHandler(selector, event, funct) {
+        $(selector).on(event, funct);
+    }
+
+    function appendToBuffer(selector, element) {
+        selectorBuffer.push(selector || "");
+        elementsBuffer.push(element || "");
+    }
+
+    return {
+        appendChild: appendChild,
+        removeChild: removeChild,
+        addHandler: addHandler,
+        appendToBuffer: appendToBuffer
+    }
+
+}());
+
+$(document).ready(function () {
+    domModule.appendChild('<div>', 'body');
+    domModule.appendChild('<ul>', 'div');
+    domModule.appendChild('<li id="1">1</li>', 'ul');
+    domModule.appendChild('<li>5<li>', 'ul');
+    domModule.appendChild('<li>', 'ul');
+    domModule.appendChild('<li>', 'ul');
+    domModule.removeChild("ul", "li:first-child");
+    domModule.addHandler("a.button", 'click', function () { alert("Clicked") });
+    domModule.appendToBuffer("container", div.cloneNode(true));
+    domModule.appendToBuffer("#main-nav ul", navItem);
+
+
+}
+)
