d8c699027e484e5036bf6532544e09bbc45bc211
Marian Graurov <marian_graurov@abv.bg>
2014-08-07 09:26:35 +0300

    1.01 fixed

--- 1.01 DOMManipulation/1.01 DOMManipulation/Script/script.js
+++ 1.01 DOMManipulation/1.01 DOMManipulation/Script/script.js
@@ -18,6 +18,13 @@
     function appendToBuffer(selector, element) {
         selectorBuffer.push(selector || "");
         elementsBuffer.push(element || "");
+        if (elementsBuffer.length >= 5) {
+            for (var i = 0; i < elementsBuffer.length; i++) {
+                appendChild(elementsBuffer[i], selectorBuffer[i]);
+            }
+            elementsBuffer = [];
+            selectorBuffer = [];
+        }
     }
 
     return {
@@ -30,16 +37,28 @@
 }());
 
 $(document).ready(function () {
-    domModule.appendChild('<div>', 'body');
-    domModule.appendChild('<ul>', 'div');
+    domModule.appendChild('<div id="experiment-div">', 'body');
+    domModule.appendChild('<div id="div-for-buffer">', 'body')
+    domModule.appendChild('<ul>', '#experiment-div');
     domModule.appendChild('<li id="1">1</li>', 'ul');
     domModule.appendChild('<li>5<li>', 'ul');
     domModule.appendChild('<li>', 'ul');
     domModule.appendChild('<li>', 'ul');
     domModule.removeChild("ul", "li:first-child");
     domModule.addHandler("a.button", 'click', function () { alert("Clicked") });
-    domModule.appendToBuffer("container", div.cloneNode(true));
-    domModule.appendToBuffer("#main-nav ul", navItem);
+    domModule.appendToBuffer("#div-for-buffer", '<div id="some-div">');
+    domModule.appendToBuffer("#some-div", '<div id="first-div">');
+    domModule.appendToBuffer("#some-div", '<div id="second-div">');
+    domModule.appendToBuffer("#first-div", '<ul id="first-list">');
+    domModule.appendToBuffer("#first-list", '<li>first li of first list</li>');
+    domModule.appendToBuffer("#first-list", '<li>second li of first list</li>');
+    domModule.appendToBuffer("#second-div", '<div id="some-div">');
+    domModule.appendToBuffer("#second-div", '<ul id="second-list">');
+    domModule.appendToBuffer("#second-list", '<li>first li of second list</li>');
+    domModule.appendToBuffer("#second-list", '<li>second li of second list</li>');
+    domModule.appendToBuffer("#second-list", '<li>third li of second list</li>');
+
+    //domModule.appendToBuffer("", );
 
 
 }
